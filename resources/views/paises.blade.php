efb29b98f4b8e7597a36da8e33d664b23eb9d93a
stive <you@example.com>
2022-05-02 16:54:37 -0500

    Evidencia_01

---
+++ resources/views/paises.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/css/bootstrap.min.css" integrity="sha512-T584yQ/tdRR5QwOpfvDfVQUidzfgc2339Lc8uBDtcp/wYu80d7jwBgAxbyMh0a9YM9F8N3tdErpFI8iaGx6x5g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Document</title>
+</head>
+<body>
+
+    <h1>Paises de la región</h1>
+
+    <table border="solid" class="table table-bordered">
+        <thead>
+            <tr>
+                <th class="text-primary">
+                    País
+                </th>
+                <th>
+                    Capital
+                </th>
+                <th>
+                    Moneda
+                </th>
+                <th>
+                    Población
+                </th>
+                <th>
+                    Ciudades
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($paises as $pais => $infopais)
+            <tr>
+                <td class="text-primary" rowspan="{{ count($infopais['ciu']) }}">
+                    {{ $pais }}
+                </td>
+                <td rowspan="{{ count($infopais['ciu']) }}">
+                    {{ $infopais["cap"] }}
+                </td>
+                <td rowspan="{{ count($infopais['ciu']) }}">
+                    {{ $infopais ["mon"] }}
+                </td>
+                <td rowspan="{{ count($infopais['ciu']) }}">
+                    {{ $infopais ["pob"] }} millones hab.
+                </td>
+                @foreach($infopais["ciu"] as $ciudad)
+                <td>
+                    {{ $ciudad }}
+                </td>
+            </tr>
+                @endforeach
+            @endforeach
+        </tbody>
+    </table>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,41 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('paises', function(){
+    //Arreglo
+    $paises = [
+        "Colombia"=> [
+            "cap" => "Bogotá",
+            "mon" => "Peso",
+            "pob" => 51,
+            "ciu" => [
+                "Medellín",
+                "Cali",
+                "Pereira"
+            ]
+        ],
+        "Ecuador"=> [
+            "cap" => "Quito",
+            "mon" => "Dólar",
+            "pob" => 17,64,
+            "ciu" => [
+                "Guayaquil",
+                "Cuenca",
+            ]
+        ],
+        "México"=>[
+            "cap" => "Ciudad México",
+            "mon" => "Pesos",
+            "pob" => 128,9,
+            "ciu" => [
+                "Guadalajara",
+                "Cancún",
+                "Cuernavaca",
+                "Sinaloa"
+            ]
+        ]
+    ];
+    return view ('paises')
+    ->with('paises', $paises);
+});
