e958151dd18a4faef7ac6b575e19dd0b4bf3de82
John <you@example.com>
2022-06-13 17:21:51 -0500

    13-06-2022

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -6,6 +6,7 @@ use App\Models\producto;
 use App\Models\Categoria;
 use App\Models\Marca;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ProductoController extends Controller
 {
@@ -45,17 +46,77 @@ class ProductoController extends Controller
      */
     public function store(Request $r)
     {
+        //Validaciones
+            //1. Establecer reglas de validación
+
+            $reglas = [
+
+                "nombre" => 'required|alpha|unique:productos,nombre',
+                "desc" => 'required|min:5|max:25',
+                "precio" => 'required|numeric',
+                "imagen" => 'required|image|',
+                "marca" => 'required',
+                "categoria" => 'required'
+                
+
+            ];
+
+            //2. Crear el objeto validador
+
+            $v = Validator::make($r->all(), $reglas );
+
+            //3. Validar
+
+            //die(var_dump($v -> errors()));]
+            
+            if($v -> fails()){
+
+                //si la validacion fallo
+                //redirigir a la vista de create(ruta: productos/create)
+
+                return redirect('productos/create')
+                         ->withErrors($v);
+
+            }else{
+
+                //validacion exitosa
+
+                //examinar archivo cargado
+
+            $archivo= $r -> imagen;
+
+            // Obtener el nombre original del archivo
+
+            $nombre_archivo = $archivo->getClientOriginalName();
+
+            //establecer la ubicacion guardado del archivo
+
+            $ruta = public_path()."/img";
+
+            //MOVER ARCHIVO IMAGEN A LA UBICACION Y NOMBRE DESEADOS
+
+            $archivo->move($ruta, $nombre_archivo);
+
+
+
         //Crear un nuevo producto:
+
         $p = new Producto();
         $p->nombre = $r->nombre;
         $p->descripción = $r->desc;
         $p->precio = $r->precio;
         $p->marca_id = $r->marca;
         $p->categoria_id = $r->categoria;
+        $p->imagen = $nombre_archivo;
         //grabar producto
         $p->save();
-        echo "producto guardado";
+        //redirigir a prductos/create
+        //con un mensaje exito
+        return redirect('productos/create')
+                 ->with('mensajito' , 'Producto registrado exitosamente');
     }
+
+            }
 
     /**
      * Display the specified resource.
--- resources/views/productos/create.blade.php
+++ resources/views/productos/create.blade.php
@@ -7,8 +7,15 @@
         padding-left: 50px;
     }
 </Style>
-        <form class="col s8" method="POST" action="{{ route('productos.store') }}">
+        <form class="col s8" method="POST" action="{{ route('productos.store') }}"
+        enctype="multipart/form-data">
           @csrf 
+          @if( session('mensajito') )
+          <div class="row">
+            <strong>{{ session('mensajito') }}</strong>
+          </div>
+
+          @endif
             <div class="row">
                 <div class="col s8">
                     <h1 class="cyan darken-3">Nuevo producto de la tienda</h1>
@@ -19,6 +26,7 @@
             <div class="input-field col s6">
               <input name="nombre" placeholder="Nombre del producto" id="nombre" type="text" class="validate">
               <label for="nombre">Nombre del producto</label>
+              <strong>{{ $errors->first('nombre') }}</strong>
             </div>
           </div>
 
@@ -26,6 +34,7 @@
             <div class="input-field col s8">
               <input name="desc" id="desc" type="text" class="validate">
               <label for="desc">Descripción</label>
+              <strong>{{ $errors->first('desc') }}</strong>
             </div>
           </div>
 
@@ -33,45 +42,52 @@
             <div class="input-field col s8">
               <input name="precio" id="precio" type="number" class="validate">
               <label for="precio">Precio</label>
+              <strong>{{ $errors->first('precio') }}</strong>
             </div>
           </div>
 
           <div class="row">
             <div class="input-field col s8">
               <select name="marca" id="marca">
-                      <option>Elija la marca</option>
+                      <option value="">Elija la marca</option>
                       @foreach ($marcas as $marca)
                           <option value="{{ $marca->id }}">
                             {{ $marca->nombre }}
                           </option>
                       @endforeach
               </select>
+              <label>Elija marca</label>
+              <strong>{{ $errors->first('marca') }}</strong>
             </div>
           </div>
 
           <div class="row">
             <div class="col s8 input-field">
               <select name="categoria" id="categoria">
+                <option value="">Elija la categoria</option>
                 @foreach($categorias as $categoria)
                   <option value="{{ $categoria->id }}">
                     {{ $categoria->nombre }}
                   </option>
                 @endforeach
               </select>
+              <label>Elija categoria</label>
+              <strong>{{ $errors->first('categoria') }}</strong>
             </div>
           </div>
 
-          <div class="fila">
-            <div class="file-field input-field">
-              <div class="btn">
-                <span>Coloca aquí la imagen del producto</span>
-                <input type="file">
-              </div>
-              <div class="file-path-wrapper">
-                <input class="file-path validate" type="text">
-              </div>
+        <div class="row">
+          <div class="file-field input-field">
+            <div class="btn">
+              <span>File</span>
+              <input type="file" name="imagen" multiple>
+            </div>
+            <div class="file-path-wrapper">
+              <input class="file-path validate" type="text" placeholder="Suba su fotitos xd">
             </div>
           </div>
+          <strong>{{ $errors->first('imagen') }}</strong>
+        </div>
           
           <div class="col s8">
           <center>
